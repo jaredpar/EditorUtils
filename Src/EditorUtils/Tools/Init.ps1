067672580b5bf0dcf6b7e0de29bdd8f036b690d3
Jared Parsons <jaredpar@microsoft.com>
2014-07-27 18:18:57 -0700

    Removed unused tools directory

--- Src/EditorUtils/Tools/Init.ps1
+++
@@ -1,7 +0,0 @@
-param($installPath, $toolsPath, $package)
-
-# Using -force here to ensure that new versions of the EditorUtils package
-# will force new versions of the psm1 script into the console.  Without 
-# -force the same version will stay resident in memory until Visual Studio
-# is restarted
-Import-Module -force (Join-Path $toolsPath "EditorUtils.psm1")
