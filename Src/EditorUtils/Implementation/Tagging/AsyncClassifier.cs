ae56af42279a74075e4cf192b8eb6c543c1ae1e8
Jared Parsons <jaredpar@microsoft.com>
2014-08-11 11:01:53 -0700

    Added async classifier

---
+++ Src/EditorUtils/Implementation/Tagging/AsyncClassifier.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Text;
+
+namespace EditorUtils.Implementation.Tagging
+{
+    internal sealed class AsyncClassifier<TData> : AsyncProducer<TData, ClassificationSpan>, IClassifier
+    {
+        private EventHandler<ClassificationChangedEventArgs> _classificationChanged;
+
+        internal AsyncClassifier(IAsyncTaggerSource<TData, ClassificationSpan> asyncTaggerSource) : base(asyncTaggerSource)
+        {
+
+        }
+
+        protected override void OnChanged(SnapshotSpan span)
+        {
+            var list = _classificationChanged;
+            if (list != null)
+            {
+                list(this, new ClassificationChangedEventArgs(span));
+            }
+        }
+
+        #region IClassifier 
+
+        event EventHandler<ClassificationChangedEventArgs> IClassifier.ClassificationChanged
+        {
+            add { _classificationChanged += value; }
+            remove { _classificationChanged -= value; }
+        }
+
+        IList<ClassificationSpan> IClassifier.GetClassificationSpans(SnapshotSpan span)
+        {
+            return GetTags(new NormalizedSnapshotSpanCollection(span)).ToList();
+        }
+
+        #endregion
+    }
+}
